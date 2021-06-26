5f110f0b3d46b01246da822df8d63aeaacbd4d6b
Ahmed Ali <ahmedali.96@hotmail.com>
2021-06-26 01:22:29 -0400

    created board item

---
+++ components/BoardItem.js
@@ -0,0 +1,128 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import { Audio } from "expo-av";
+
+export default class BoardItem extends Component {
+  _isMounted = false;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isPlaying: false,
+      playbackInstance: null,
+      volume: 1.0,
+      isBuffering: true,
+    };
+    this.playbackInstance = new Audio.Sound();
+  }
+
+  async componentDidMount() {
+    this._isMounted = true;
+
+    if (this._isMounted) {
+      try {
+        await Audio.setAudioModeAsync({
+          allowsRecordingIOS: false,
+          interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+          playsInSilentModeIOS: true,
+          shouldDuckAndroid: true,
+          interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+          playThroughEarpieceAndroid: false,
+          staysActiveInBackground: true,
+        });
+
+        await this.loadAudio();
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
+    this.playbackInstance.unloadAsync();
+  }
+
+  async loadAudio() {
+    if (this._isMounted) {
+      const { isPlaying, volume } = this.state;
+      const { src } = this.props;
+
+      try {
+        const source = {
+          uri: src,
+        };
+
+        const status = {
+          shouldPlay: isPlaying,
+          volume: volume,
+        };
+
+        this.playbackInstance.setOnPlaybackStatusUpdate(
+          this.onPlaybackStatusUpdate
+        );
+        await this.playbackInstance.loadAsync(source, status, false);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }
+
+  onPlaybackStatusUpdate = async (status) => {
+    if (this._isMounted) {
+      this.setState({
+        isBuffering: status.isBuffering,
+      });
+
+      if (status.didJustFinish === true) {
+        // audio has finished!
+        this.setState({
+          isPlaying: false,
+        });
+        await this.playbackInstance.stopAsync();
+        await this.playbackInstance.unloadAsync();
+
+        await this.loadAudio();
+      }
+    }
+  };
+
+  handlePlayPause = async () => {
+    if (this._isMounted) {
+      const { isPlaying } = this.state;
+      isPlaying
+        ? await this.playbackInstance.pauseAsync()
+        : await this.playbackInstance.playAsync();
+
+      return this.setState({
+        isPlaying: !isPlaying,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <TouchableOpacity style={styles.cont} onPress={this.handlePlayPause}>
+        {this.state.isPlaying ? (
+          <FontAwesome name="pause-circle" size={20} />
+        ) : (
+          <FontAwesome name="play-circle" size={20} />
+        )}
+        <Text>Key Logged: </Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  cont: {
+    border: "2px solid black",
+    margin: 10,
+    height: 80,
+    width: 100,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
